83582e9b0fb22d958d1c804b274c238015e5e035
Semyon Kirekov <semyon@kirekov.com>
2024-05-13 21:59:36 +0300

    fix ExpenseNote

--- plyushkin-backend/src/main/kotlin/com/plyushkin/expense/ExpenseNote.kt
+++ plyushkin-backend/src/main/kotlin/com/plyushkin/expense/ExpenseNote.kt
@@ -6,10 +6,10 @@ import com.plyushkin.wallet.WalletId
 
 data class ExpenseNoteId private constructor(val value: Long) {
     companion object {
-        fun create(value: Long): Either<InvalidExpenseNoteId, ExpenseNoteId> {
+        fun create(value: Long): Either<Invalid, ExpenseNoteId> {
             return value.let {
                 if (it <= 0) {
-                    Either.Left(InvalidExpenseNoteId("Value should be positive: $value"))
+                    Either.Left(Invalid("Value should be positive: $value"))
                 } else {
                     Either.Right(ExpenseNoteId(value))
                 }
@@ -17,11 +17,29 @@ data class ExpenseNoteId private constructor(val value: Long) {
         }
     }
 
-    class InvalidExpenseNoteId(message: String) : IllegalArgumentException(message)
+    class Invalid(message: String) : IllegalArgumentException(message)
 }
 
-class ExpenseNote(val id: ExpenseNoteId, val walletId: WalletId, val whoDid: UserId) {
+data class CategoryId private constructor(val value: Long) {
+    companion object {
+        fun create(value: Long): Either<Invalid, CategoryId> {
+            return value.let {
+                if (it <= 0) {
+                    Either.Left(Invalid("Value should be positive: $value"))
+                } else {
+                    Either.Right(CategoryId(value))
+                }
+            }
+        }
+    }
 
+    class Invalid(message: String) : IllegalArgumentException(message)
+}
+
+class ExpenseNote(val id: ExpenseNoteId,
+                  val walletId: WalletId,
+                  val whoDid: UserId,
+                  val categoryId: CategoryId) {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
@@ -29,11 +47,37 @@ class ExpenseNote(val id: ExpenseNoteId, val walletId: WalletId, val whoDid: Use
         other as ExpenseNote
 
         if (id != other.id) return false
+        if (walletId != other.walletId) return false
 
         return true
     }
 
     override fun hashCode(): Int {
-        return id.hashCode()
+        var result = id.hashCode()
+        result = 31 * result + walletId.hashCode()
+        return result
+    }
+}
+
+class Category(val id: CategoryId,
+               val walletId: WalletId,
+               val whoCreated: UserId,
+               val parent: Category?) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Category
+
+        if (id != other.id) return false
+        if (walletId != other.walletId) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id.hashCode()
+        result = 31 * result + walletId.hashCode()
+        return result
     }
 }
