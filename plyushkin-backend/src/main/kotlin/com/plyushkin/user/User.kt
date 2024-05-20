7c2ada43af8ad5252500a12b4849b291f16f1b07
Semyon Kirekov <semyon@kirekov.com>
2024-05-20 21:05:47 +0300

    refactor UserId and User

--- plyushkin-backend/src/main/kotlin/com/plyushkin/user/User.kt
+++ plyushkin-backend/src/main/kotlin/com/plyushkin/user/User.kt
@@ -1,51 +1,39 @@
 package com.plyushkin.user
 
 import arrow.core.Either
+import com.plyushkin.common.BaseEntity
+import com.plyushkin.common.PrefixedId
 import com.plyushkin.wallet.WalletId
-import java.util.concurrent.ThreadLocalRandom
 
-data class UserId private constructor(val value: String) {
+class UserId : PrefixedId {
+    private constructor() : super("U")
+
+    private constructor(value: String) : super("U", value)
+
     companion object {
-        fun createRandom(): UserId {
-            val randomNum = ThreadLocalRandom.current().nextLong(0, Long.MAX_VALUE)
-            return create("U$randomNum").getOrNull()!!
-        }
+        fun createRandom(): UserId = UserId()
 
         fun create(value: String): Either<InvalidUserId, UserId> {
-            if (!value.startsWith("U")) {
-                return Either.Left(InvalidUserId("UserId must start with U: $value"))
-            }
-            return value.substring(3).toLongOrNull().let {
-                if (it == null) {
-                    Either.Left(InvalidUserId("UserId must contain a number: $value"))
-                } else {
-                    Either.Right(UserId(value))
-                }
+            return try {
+                Either.Right(UserId(value))
+            } catch (e: Invalid) {
+                Either.Left(
+                    InvalidUserId(
+                        "Cannot create UserId with invalid value: $value",
+                        e
+                    )
+                )
             }
         }
     }
 
-    class InvalidUserId(message: String) : IllegalArgumentException(message)
+    class InvalidUserId(message: String, cause: Throwable) : Invalid(message, cause)
 }
 
 enum class Permission {
     READ, WRITE
 }
 
-class User(val id: UserId, val permissions: Map<WalletId, Set<Permission>>) {
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as User
-
-        if (id != other.id) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return id.hashCode()
-    }
+class User(id: UserId, val permissions: Map<WalletId, Set<Permission>>) :
+    BaseEntity<UserId>(id) {
 }
