170e4393d1116e0325187ef58de18d143f59af57
Semyon Kirekov <semyon@kirekov.com>
2024-05-20 21:10:18 +0300

    fix BaseEntity

--- plyushkin-backend/src/main/kotlin/com/plyushkin/common/base.kt
+++ plyushkin-backend/src/main/kotlin/com/plyushkin/common/base.kt
@@ -2,7 +2,7 @@ package com.plyushkin.common
 
 import java.util.concurrent.ThreadLocalRandom
 
-open class BaseEntity<T>(val id: T) {
+open class BaseEntity<T>(private val id: T) {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
