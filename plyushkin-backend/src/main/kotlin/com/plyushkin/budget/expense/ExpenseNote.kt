7ec1951f1bdc906dde622d0ebf6ff2c19eb48664
Semyon Kirekov <semyon@kirekov.com>
2024-05-23 22:48:44 +0300

    fix ExpenseNote.kt

--- plyushkin-backend/src/main/kotlin/com/plyushkin/budget/expense/ExpenseNote.kt
+++ plyushkin-backend/src/main/kotlin/com/plyushkin/budget/expense/ExpenseNote.kt
@@ -2,11 +2,12 @@ package com.plyushkin.budget.expense
 
 import arrow.core.Either
 import com.plyushkin.budget.Money
+import com.plyushkin.common.DomainContext
 import com.plyushkin.user.UserId
 import com.plyushkin.wallet.WalletId
 import java.time.LocalDate
 
-class ExpenseNote internal constructor(
+data class ExpenseNote internal constructor(
     val id: ExpenseNoteId,
     val walletId: WalletId,
     val whoDid: UserId,
@@ -15,28 +16,7 @@ class ExpenseNote internal constructor(
     val category: ExpenseNoteCategory,
     val comment: String
 ) {
-
-
-
-    companion object {
-        fun create(
-            id: ExpenseNoteId,
-            walletId: WalletId,
-            whoDid: UserId,
-            date: LocalDate,
-            amount: Money,
-            category: ExpenseNoteCategory,
-            comment: String
-        ): Either<InvalidExpenseNote, ExpenseNote> {
-            if (category.walletId != walletId) {
-                return Either.Left(InvalidCategory("Category walletId=${category.walletId} does not equal to ExpenseNote walletId=$walletId"))
-            }
-            if (category.children.isNotEmpty()) {
-                return Either.Left(InvalidCategory("Category is not terminated because it has children: ${category.children}"))
-            }
-            return Either.Right(ExpenseNote(id, walletId, whoDid, date, amount, category, comment))
-        }
-    }
+    companion object;
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
@@ -55,10 +35,45 @@ class ExpenseNote internal constructor(
         result = 31 * result + walletId.hashCode()
         return result
     }
+
+    open class InvalidExpenseNoteException(message: String) : IllegalArgumentException(message)
+
+    class InvalidCategoryException(message: String) : InvalidExpenseNoteException(message)
 }
 
-open class InvalidExpenseNote(message: String) : IllegalArgumentException(message)
+fun ExpenseNote.Companion.create(
+    id: ExpenseNoteId,
+    walletId: WalletId,
+    whoDid: UserId,
+    date: LocalDate,
+    amount: Money,
+    category: ExpenseNoteCategory,
+    comment: String
+): Either<ExpenseNote.InvalidExpenseNoteException, ExpenseNote> =
+    ExpenseNote(id, walletId, whoDid, date, amount, category, comment)
+        .validateCategory()
 
-class InvalidCategory(message: String) : InvalidExpenseNote(message)
+private fun ExpenseNote.validateCategory(): Either<ExpenseNote.InvalidCategoryException, ExpenseNote> {
+    if (category.walletId != walletId) {
+        return Either.Left(ExpenseNote.InvalidCategoryException("Category walletId=${category.walletId} does not equal to ExpenseNote walletId=$walletId"))
+    }
+    if (category.children.isNotEmpty()) {
+        return Either.Left(ExpenseNote.InvalidCategoryException("Category cannot be assigned for expense note because it has children: ${category.children}"))
+    }
+    return Either.Right(this)
+}
 
+fun ExpenseNote.domainContext(): DomainContext<ExpenseNote> = DomainContext(this, emptyList())
+
+fun DomainContext<ExpenseNote>.update(
+    date: LocalDate,
+    amount: Money,
+    category: ExpenseNoteCategory,
+    comment: String
+): Either<ExpenseNote.InvalidExpenseNoteException, DomainContext<ExpenseNote>> {
+    return entity.run {
+        copy(date = date, amount = amount, category = category, comment = comment)
+            .validateCategory()
+    }.map { copy(entity = it) }
+}
 
