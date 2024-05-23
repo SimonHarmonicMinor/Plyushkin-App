81c1f64bb1354e4b4268b742e055289c847b65a0
Semyon Kirekov <semyon@kirekov.com>
2024-05-23 22:33:13 +0300

    fix Money.kt

--- plyushkin-backend/src/main/kotlin/com/plyushkin/budget/Money.kt
+++ plyushkin-backend/src/main/kotlin/com/plyushkin/budget/Money.kt
@@ -3,27 +3,21 @@ package com.plyushkin.budget
 import arrow.core.Either
 import java.math.BigDecimal
 
-data class Money private constructor(val value: BigDecimal) {
-    companion object {
-        fun zero(): Money {
-            return Money(BigDecimal.ZERO)
-        }
+data class Money internal constructor(val value: BigDecimal) {
+    companion object
 
-        fun ofLong(value: Long): Either<Invalid, Money> {
-            return ofBigDecimal(BigDecimal.valueOf(value))
-        }
-
-        fun ofBigDecimal(value: BigDecimal): Either<Invalid, Money> {
-            if (value < BigDecimal.ZERO) {
-                return Either.Left(Invalid("Amount of money cannot be less than zero: $value"))
-            }
-            return Either.Right(Money(value))
-        }
-    }
-
-    fun plus(amount: Money): Money {
-        return Money(value.plus(amount.value))
-    }
-
-    class Invalid(message: String) : IllegalArgumentException(message)
+    class InvalidException(message: String) : IllegalArgumentException(message)
 }
+
+fun Money.Companion.zero(): Money = Money(BigDecimal.ZERO)
+
+fun Money.Companion.ofLong(value: Long): Either<Money.InvalidException, Money> = ofBigDecimal(BigDecimal.valueOf(value))
+
+fun Money.Companion.ofBigDecimal(value: BigDecimal): Either<Money.InvalidException, Money> =
+    if (value < BigDecimal.ZERO) {
+        Either.Left(Money.InvalidException("Amount of money cannot be less than zero: $value"))
+    } else {
+        Either.Right(Money(value))
+    }
+
+fun Money.plus(amount: Money): Money = Money(value.plus(amount.value))
