1636492db3cbc0bc9a7a8efc731a1bb7400c462b
Semyon Kirekov <semyon@kirekov.com>
2024-06-02 22:48:23 +0300

    add UserId

---
+++ plyushkin-service/src/main/java/com/plyushkin/user/UserId.java
@@ -0,0 +1,47 @@
+package com.plyushkin.user;
+
+import static lombok.AccessLevel.PROTECTED;
+
+import com.plyushkin.common.PrefixedId;
+import jakarta.persistence.Embeddable;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Embeddable
+@NoArgsConstructor(access = PROTECTED)
+public class UserId extends PrefixedId {
+
+  protected UserId(@NonNull String prefix) {
+    super(prefix);
+  }
+
+  protected UserId(@NonNull String prefix, @NonNull String value)
+      throws InvalidPrefixedIdException {
+    super(prefix, value);
+  }
+
+  public static UserId create(String value) throws InvalidUserIdException {
+    try {
+      return new UserId("U", value);
+    } catch (InvalidPrefixedIdException e) {
+      throw new InvalidUserIdException(
+          "Invalid UserId", e
+      );
+    }
+  }
+
+  public static UserId createRandom() {
+    return new UserId("U");
+  }
+
+  public static class InvalidUserIdException extends Exception {
+
+    public InvalidUserIdException(String message) {
+      super(message);
+    }
+
+    public InvalidUserIdException(String message, Throwable cause) {
+      super(message, cause);
+    }
+  }
+}
