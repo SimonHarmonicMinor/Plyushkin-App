ce304678310c45b3f297db7332cfab960d5805ee
Semyon Kirekov <semyon@kirekov.com>
2024-06-08 21:10:36 +0300

    add Currency

---
+++ plyushkin-service/src/main/java/com/plyushkin/wallet/Currency.java
@@ -0,0 +1,36 @@
+package com.plyushkin.wallet;
+
+public interface Currency {
+
+  String value();
+
+  static Currency of(String value) {
+    try {
+      return CurrencyEnum.of(value);
+    } catch (IllegalArgumentException e) {
+      return new StringCurrency(value);
+    }
+  }
+}
+
+enum CurrencyEnum implements Currency {
+  RUB, DOLLAR, EURO;
+
+  public static CurrencyEnum of(String value) {
+    for (CurrencyEnum currencyEnum : values()) {
+      if (currencyEnum.name().equals(value)) {
+        return currencyEnum;
+      }
+    }
+    throw new IllegalArgumentException("Unknown value: " + value);
+  }
+
+  @Override
+  public String value() {
+    return name();
+  }
+}
+
+record StringCurrency(String value) implements Currency {
+
+}
