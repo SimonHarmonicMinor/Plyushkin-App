068a302770294a7fb5c928d903325a166866b43a
Semyon Kirekov <semyon@kirekov.com>
2024-06-08 20:24:31 +0300

    add IncomeNoteId

---
+++ plyushkin-service/src/main/java/com/plyushkin/budget/income/IncomeNoteId.java
@@ -0,0 +1,40 @@
+package com.plyushkin.budget.income;
+
+import static lombok.AccessLevel.PROTECTED;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@NoArgsConstructor(access = PROTECTED)
+@EqualsAndHashCode
+public class IncomeNoteId {
+
+  @Column(name = "id")
+  private long value;
+
+  public static IncomeNoteId create(long value) throws InvalidIncomeNoteIdException {
+    if (value <= 0) {
+      throw new InvalidIncomeNoteIdException(
+          "Value should be positive but it is: " + value
+      );
+    }
+    IncomeNoteId incomeNoteId = new IncomeNoteId();
+    incomeNoteId.value = value;
+    return incomeNoteId;
+  }
+
+  @Override
+  public String toString() {
+    return String.valueOf(value);
+  }
+
+  public static class InvalidIncomeNoteIdException extends Exception {
+
+    public InvalidIncomeNoteIdException(String message) {
+      super(message);
+    }
+  }
+}
