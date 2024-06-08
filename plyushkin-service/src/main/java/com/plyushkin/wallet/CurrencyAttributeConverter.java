7582ac0e4b3766a0420960010b7428a5893e4b67
Semyon Kirekov <semyon@kirekov.com>
2024-06-08 21:10:43 +0300

    add CurrencyAttributeConverter

---
+++ plyushkin-service/src/main/java/com/plyushkin/wallet/CurrencyAttributeConverter.java
@@ -0,0 +1,18 @@
+package com.plyushkin.wallet;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter
+class CurrencyAttributeConverter implements AttributeConverter<Currency, String> {
+
+  @Override
+  public String convertToDatabaseColumn(Currency attribute) {
+    return attribute.value();
+  }
+
+  @Override
+  public Currency convertToEntityAttribute(String dbData) {
+    return Currency.of(dbData);
+  }
+}
