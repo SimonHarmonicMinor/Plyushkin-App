1ecb48e7f8402f94cce9bd5c5debefd3a64f0445
Semyon Kirekov <semyon@kirekov.com>
2024-06-08 21:01:59 +0300

    add EqualsAndHashCode

--- plyushkin-service/src/main/java/com/plyushkin/wallet/WalletId.java
+++ plyushkin-service/src/main/java/com/plyushkin/wallet/WalletId.java
@@ -4,11 +4,13 @@ import static lombok.AccessLevel.PROTECTED;
 
 import com.plyushkin.common.PrefixedId;
 import jakarta.persistence.Embeddable;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
 @Embeddable
 @NoArgsConstructor(access = PROTECTED)
+@EqualsAndHashCode
 public class WalletId extends PrefixedId {
 
   private WalletId(@NonNull String prefix) {
