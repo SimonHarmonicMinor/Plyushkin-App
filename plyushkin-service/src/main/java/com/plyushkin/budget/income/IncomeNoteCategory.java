050be8ce6280c35ded8c29eb79ee959db2191c86
Semyon Kirekov <semyon@kirekov.com>
2024-06-08 20:39:32 +0300

    add IncomeNoteCategory

---
+++ plyushkin-service/src/main/java/com/plyushkin/budget/income/IncomeNoteCategory.java
@@ -0,0 +1,32 @@
+package com.plyushkin.budget.income;
+
+import static lombok.AccessLevel.PROTECTED;
+
+import com.plyushkin.budget.AbstractCategory;
+import com.plyushkin.user.UserId;
+import com.plyushkin.wallet.WalletId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import java.util.HashSet;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "income_note_category")
+@NoArgsConstructor(access = PROTECTED)
+public class IncomeNoteCategory extends AbstractCategory<IncomeNoteCategoryId, IncomeNoteCategory> {
+
+  public static IncomeNoteCategory create(
+      IncomeNoteCategoryId id,
+      String name,
+      WalletId walletId,
+      UserId whoCreated
+  ) {
+    IncomeNoteCategory incomeNoteCategory = new IncomeNoteCategory();
+    incomeNoteCategory.id = id;
+    incomeNoteCategory.name = name;
+    incomeNoteCategory.walletId = walletId;
+    incomeNoteCategory.whoCreated = whoCreated;
+    incomeNoteCategory.children = new HashSet<>();
+    return incomeNoteCategory;
+  }
+}
