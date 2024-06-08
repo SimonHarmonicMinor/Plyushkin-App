258ed74fd9c540918ebaf456028539f8d1bac0c9
Semyon Kirekov <semyon@kirekov.com>
2024-06-08 21:33:05 +0300

    fill User

--- plyushkin-service/src/main/java/com/plyushkin/user/User.java
+++ plyushkin-service/src/main/java/com/plyushkin/user/User.java
@@ -1,18 +1,22 @@
 package com.plyushkin.user;
 
-import static lombok.AccessLevel.PRIVATE;
+import static lombok.AccessLevel.PROTECTED;
 
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import lombok.AccessLevel;
+import java.util.List;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "users")
-@NoArgsConstructor(access = PRIVATE)
+@NoArgsConstructor(access = PROTECTED)
 public class User {
 
   @EmbeddedId
   private UserId id;
+
+  @OneToMany(mappedBy = "user")
+  private List<Profile> profiles;
 }
