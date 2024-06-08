cb3f684934ba1d2764a349eca5e59088be7eb6cf
Semyon Kirekov <semyon@kirekov.com>
2024-06-08 21:10:52 +0300

    fill Wallet

--- plyushkin-service/src/main/java/com/plyushkin/wallet/Wallet.java
+++ plyushkin-service/src/main/java/com/plyushkin/wallet/Wallet.java
@@ -1,5 +1,37 @@
 package com.plyushkin.wallet;
 
+import static lombok.AccessLevel.PACKAGE;
+import static lombok.AccessLevel.PROTECTED;
+
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "wallet")
+@NoArgsConstructor(access = PROTECTED)
+@Getter(PACKAGE)
 public class Wallet {
 
+  @EmbeddedId
+  @AttributeOverride(name = "value", column = @Column(name = "id"))
+  private WalletId id;
+
+  private String name;
+
+  @Convert(converter = CurrencyAttributeConverter.class)
+  private Currency currency;
+
+  public static Wallet create(WalletId id, String name, Currency currency) {
+    Wallet wallet = new Wallet();
+    wallet.id = id;
+    wallet.name = name;
+    wallet.currency = currency;
+    return wallet;
+  }
 }
