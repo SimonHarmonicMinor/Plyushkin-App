bf161bb11d2a61b725cb6b13451a9965c356f46e
Semyon Kirekov <semyon@kirekov.com>
2024-06-09 23:29:57 +0300

    fix migrations

--- plyushkin-service/src/main/resources/db/migration/V2__create_wallet_tables.sql
+++ plyushkin-service/src/main/resources/db/migration/V2__create_wallet_tables.sql
@@ -1,11 +1,9 @@
 CREATE SCHEMA wallet;
 
-CREATE DOMAIN wallet.WALLET_ID AS VARCHAR(50);
-
 CREATE TABLE wallet.wallet
 (
-    id         wallet.WALLET_ID PRIMARY KEY,
+    id         VARCHAR(50) PRIMARY KEY,
     name       VARCHAR(200) NOT NULL,
     currency   VARCHAR(50)  NOT NULL,
-    created_by auth.USER_ID NOT NULL REFERENCES auth.users (id)
+    created_by VARCHAR(50) NOT NULL REFERENCES auth.users (id)
 );
--- plyushkin-service/src/main/resources/db/migration/V3__create_budget_tables.sql
+++ plyushkin-service/src/main/resources/db/migration/V3__create_budget_tables.sql
@@ -1,51 +1,51 @@
 CREATE SCHEMA budget;
 
-CREATE DOMAIN budget.EXPENSE_NOTE_ID AS VARCHAR(50);
-
-CREATE DOMAIN budget.EXPENSE_NOTE_CATEGORY_ID AS VARCHAR(50);
-
-CREATE DOMAIN budget.INCOME_NOTE_ID AS VARCHAR(50);
-
-CREATE DOMAIN budget.INCOME_NOTE_CATEGORY_ID AS VARCHAR(50);
-
 CREATE TABLE budget.expense_note_category
 (
-    id             budget.EXPENSE_NOTE_CATEGORY_ID NOT NULL UNIQUE,
-    name           VARCHAR(200)                    NOT NULL,
-    wallet_id      wallet.WALLET_ID                NOT NULL REFERENCES wallet.wallet (id),
-    who_created_id auth.USER_ID                    NOT NULL REFERENCES auth.users (id),
-    parent_id      budget.EXPENSE_NOTE_CATEGORY_ID REFERENCES budget.expense_note_category (id),
-    UNIQUE (wallet_id, name)
+    pk             BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    number         BIGINT       NOT NULL,
+    name           VARCHAR(200) NOT NULL,
+    wallet_id      VARCHAR(50)  NOT NULL REFERENCES wallet.wallet (id),
+    who_created_id VARCHAR(50)  NOT NULL REFERENCES auth.users (id),
+    parent_id      BIGINT REFERENCES budget.expense_note_category (pk),
+    UNIQUE (wallet_id, name),
+    UNIQUE (wallet_id, number)
 );
 
 CREATE TABLE budget.expense_note
 (
-    id          budget.EXPENSE_NOTE_ID          NOT NULL UNIQUE,
-    wallet_id   wallet.WALLET_ID                NOT NULL REFERENCES wallet.wallet (id),
-    who_did_id  auth.USER_ID                    NOT NULL REFERENCES auth.users (id),
-    date        DATE                            NOT NULL,
-    amount      BIGINT                          NOT NULL,
-    category_id budget.EXPENSE_NOTE_CATEGORY_ID NOT NULL REFERENCES budget.expense_note_category (id),
-    comment     VARCHAR(500)                    NOT NULL
+    pk          BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    number      BIGINT          NOT NULL,
+    wallet_id   VARCHAR(50)     NOT NULL REFERENCES wallet.wallet (id),
+    who_did_id  VARCHAR(50)     NOT NULL REFERENCES auth.users (id),
+    date        DATE            NOT NULL,
+    amount      NUMERIC(33, 18) NOT NULL,
+    category_id BIGINT          NOT NULL REFERENCES budget.expense_note_category (pk),
+    comment     VARCHAR(500)    NOT NULL,
+    UNIQUE (wallet_id, number)
 );
 
 CREATE TABLE budget.income_note_category
 (
-    id             budget.INCOME_NOTE_CATEGORY_ID NOT NULL UNIQUE,
-    name           VARCHAR(200)                   NOT NULL,
-    wallet_id      wallet.WALLET_ID               NOT NULL REFERENCES wallet.wallet (id),
-    who_created_id auth.USER_ID                   NOT NULL REFERENCES auth.users (id),
-    parent_id      budget.INCOME_NOTE_CATEGORY_ID REFERENCES budget.income_note_category (id),
-    UNIQUE (wallet_id, name)
+    pk             BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    number         BIGINT       NOT NULL,
+    name           VARCHAR(200) NOT NULL,
+    wallet_id      VARCHAR(50)  NOT NULL REFERENCES wallet.wallet (id),
+    who_created_id VARCHAR(50)  NOT NULL REFERENCES auth.users (id),
+    parent_id      BIGINT REFERENCES budget.income_note_category (pk),
+    UNIQUE (wallet_id, name),
+    UNIQUE (wallet_id, number)
 );
 
 CREATE TABLE budget.income_note
 (
-    id          budget.INCOME_NOTE_ID          NOT NULL UNIQUE,
-    wallet_id   wallet.WALLET_ID               NOT NULL REFERENCES wallet.wallet (id),
-    who_did_id  auth.USER_ID                   NOT NULL REFERENCES auth.users (id),
-    date        DATE                           NOT NULL,
-    amount      BIGINT                         NOT NULL,
-    category_id budget.INCOME_NOTE_CATEGORY_ID NOT NULL REFERENCES budget.income_note_category (id),
-    comment     VARCHAR(500)                   NOT NULL
+    pk          BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    number      BIGINT          NOT NULL,
+    wallet_id   VARCHAR(50)     NOT NULL REFERENCES wallet.wallet (id),
+    who_did_id  VARCHAR(50)     NOT NULL REFERENCES auth.users (id),
+    date        DATE            NOT NULL,
+    amount      NUMERIC(33, 18) NOT NULL,
+    category_id BIGINT          NOT NULL REFERENCES budget.income_note_category (pk),
+    comment     VARCHAR(500)    NOT NULL,
+    UNIQUE (wallet_id, number)
 );
