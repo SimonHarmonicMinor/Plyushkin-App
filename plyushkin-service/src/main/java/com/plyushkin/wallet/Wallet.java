cd965535c4227bdfb372d500544669065877e974
Semyon Kirekov <semyon@kirekov.com>
2024-06-09 20:45:33 +0300

    add Wallet.createdBy

--- plyushkin-service/src/main/java/com/plyushkin/wallet/Wallet.java
+++ plyushkin-service/src/main/java/com/plyushkin/wallet/Wallet.java
@@ -3,7 +3,11 @@ package com.plyushkin.wallet;
 import static lombok.AccessLevel.PACKAGE;
 import static lombok.AccessLevel.PROTECTED;
 
+import com.plyushkin.user.UserId;
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.Column;
 import jakarta.persistence.Convert;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
@@ -23,6 +27,10 @@ public class Wallet {
 
   @Convert(converter = CurrencyAttributeConverter.class)
   private Currency currency;
+
+  @Embedded
+  @AttributeOverride(name = "value", column = @Column(name = "created_by", updatable = false))
+  private UserId createdBy;
 
   public static Wallet create(WalletId id, String name, Currency currency) {
     Wallet wallet = new Wallet();
