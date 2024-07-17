b8d4d205271dc5abb22b8676a14df826c280abd7
Semyon Kirekov <semyon@kirekov.com>
2024-07-18 00:00:12 +0300

    add AbstractNumber

---
+++ plyushkin-service/src/main/java/com/plyushkin/budget/AbstractNumber.java
@@ -0,0 +1,44 @@
+package com.plyushkin.budget;
+
+import com.plyushkin.budget.investment.DepositNumber;
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.StandardException;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@MappedSuperclass
+@NoArgsConstructor(access = PROTECTED)
+@EqualsAndHashCode
+@Getter
+public abstract class AbstractNumber implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Column(name = "number", updatable = false)
+    private long value;
+
+    protected AbstractNumber(long value) throws InvalidNumberException {
+        if (value <= 0) {
+            throw new InvalidNumberException(
+                    "Value should be positive but it is: " + value
+            );
+        }
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(value);
+    }
+
+    @StandardException
+    public static class InvalidNumberException extends Exception {
+    }
+}
