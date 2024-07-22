8a191185a78033ebd2c7b3758124c80593ad34ff
Semyon Kirekov <semyon@kirekov.com>
2024-07-22 22:27:02 +0300

    remove Currency from wallet

--- plyushkin-service/src/main/java/com/plyushkin/wallet/Wallet.java
+++ plyushkin-service/src/main/java/com/plyushkin/wallet/Wallet.java
@@ -1,19 +1,13 @@
 package com.plyushkin.wallet;
 
-import static lombok.AccessLevel.PACKAGE;
-import static lombok.AccessLevel.PROTECTED;
-
 import com.plyushkin.user.UserId;
-import jakarta.persistence.AttributeOverride;
-import jakarta.persistence.Column;
-import jakarta.persistence.Convert;
-import jakarta.persistence.Embedded;
-import jakarta.persistence.EmbeddedId;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.DynamicUpdate;
+
+import static lombok.AccessLevel.PACKAGE;
+import static lombok.AccessLevel.PROTECTED;
 
 @Entity
 @Table(schema = "wallet", name = "wallet")
@@ -27,23 +21,18 @@ public class Wallet {
 
     private String name;
 
-    @Convert(converter = CurrencyAttributeConverter.class)
-    private Currency currency;
-
     @Embedded
     @AttributeOverride(name = "value", column = @Column(name = "created_by", updatable = false))
     private UserId createdBy;
 
-    public static Wallet create(WalletId id, String name, Currency currency) {
+    public static Wallet create(WalletId id, String name) {
         Wallet wallet = new Wallet();
         wallet.id = id;
         wallet.name = name;
-        wallet.currency = currency;
         return wallet;
     }
 
-    public void update(String name, Currency currency) {
+    public void update(String name) {
         this.name = name;
-        this.currency = currency;
     }
 }
