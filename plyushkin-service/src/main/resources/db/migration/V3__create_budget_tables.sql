71fdae6a504c9183b71a7238b4d4009478fe99e4
Semyon Kirekov <semyon@kirekov.com>
2024-07-22 22:28:08 +0300

    add budget.deposit table to migration

--- plyushkin-service/src/main/resources/db/migration/V3__create_budget_tables.sql
+++ plyushkin-service/src/main/resources/db/migration/V3__create_budget_tables.sql
@@ -49,3 +49,17 @@ CREATE TABLE budget.income_note
     comment     VARCHAR(500)    NOT NULL,
     UNIQUE (wallet_id, number)
 );
+
+CREATE TABLE budget.deposit
+(
+    pk             BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    number         BIGINT          NOT NULL,
+    name           VARCHAR(200)    NOT NULL,
+    comment        VARCHAR(500)    NOT NULL,
+    currency       VARCHAR(50)     NOT NULL,
+    wallet_id      BIGINT          NOT NULL REFERENCES wallet.wallet (id),
+    initial_amount NUMERIC(33, 18) NOT NULL,
+    initial_date   DATE            NOT NULL,
+    closed_amount  NUMERIC(33, 18),
+    closed_date    DATE
+)
