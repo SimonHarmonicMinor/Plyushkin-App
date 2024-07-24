b9b51b4bfb26431b82c44b3b08ddc319e185774c
Semyon Kirekov <semyon@kirekov.com>
2024-07-24 23:41:37 +0300

    fix jpaconfig

--- plyushkin-service/src/main/java/com/plyushkin/infra/JpaConfig.java
+++ plyushkin-service/src/main/java/com/plyushkin/infra/JpaConfig.java
@@ -5,6 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
-@EnableJpaRepositories(repositoryFactoryBeanClass = EntityGraphJpaRepositoryFactoryBean.class)
+@EnableJpaRepositories(value = "com.plyushkin", repositoryFactoryBeanClass = EntityGraphJpaRepositoryFactoryBean.class)
 public class JpaConfig {
 }
