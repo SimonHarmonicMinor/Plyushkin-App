d3edf028c6264b2a27f44bc1e2143c6ea22005fe
Semyon Kirekov <semyon@kirekov.com>
2024-07-29 23:25:36 +0300

    WriteTransactional propagation

--- plyushkin-service/src/main/java/com/plyushkin/util/WriteTransactional.java
+++ plyushkin-service/src/main/java/com/plyushkin/util/WriteTransactional.java
@@ -4,10 +4,13 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Retention;
 
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Retention(RUNTIME)
 @Transactional(rollbackFor = Throwable.class)
 public @interface WriteTransactional {
-
+    @AliasFor(annotation = Transactional.class, attribute = "propagation")
+    Propagation propagation() default Propagation.REQUIRED;
 }
