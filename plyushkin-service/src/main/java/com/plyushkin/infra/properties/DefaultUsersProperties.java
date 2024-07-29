7e107097987f3c48c62405697532e207b6265708
Semyon Kirekov <semyon@kirekov.com>
2024-07-29 22:58:14 +0300

    add DefaultUsersProperties

---
+++ plyushkin-service/src/main/java/com/plyushkin/infra/properties/DefaultUsersProperties.java
@@ -0,0 +1,17 @@
+package com.plyushkin.infra.properties;
+
+import com.plyushkin.user.UserId;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
+
+import java.util.Map;
+
+@ConfigurationProperties("default-users")
+public record DefaultUsersProperties(
+        @NestedConfigurationProperty
+        Map<UserId, Info> values
+) {
+
+    public record Info(String username, String password) {
+    }
+}
