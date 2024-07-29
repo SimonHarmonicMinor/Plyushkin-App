a7d7d61d4331199bf6b992efb7f3f7fdbde606b8
Semyon Kirekov <semyon@kirekov.com>
2024-07-29 23:52:53 +0300

    fix open api generate

--- plyushkin-service/build.gradle.kts
+++ plyushkin-service/build.gradle.kts
@@ -115,7 +115,8 @@ openApi {
             "--spring.datasource.driverClassName=org.h2.Driver",
             "--spring.datasource.username=sa",
             "--spring.datasource.password=password",
-            "--spring.jpa.database-platform=org.hibernate.dialect.H2Dialect"
+            "--spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+            "--default-users.enable-creation=false"
         ))
     }
 }
