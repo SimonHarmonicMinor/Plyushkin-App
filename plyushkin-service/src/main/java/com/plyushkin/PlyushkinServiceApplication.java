7513728cf8f7c085bc4b023a9812e8508cd6807d
Semyon Kirekov <semyon@kirekov.com>
2024-07-29 22:57:14 +0300

    add configuration properties scan

--- plyushkin-service/src/main/java/com/plyushkin/PlyushkinServiceApplication.java
+++ plyushkin-service/src/main/java/com/plyushkin/PlyushkinServiceApplication.java
@@ -2,8 +2,12 @@ package com.plyushkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
+@ConfigurationPropertiesScan("com.plyushkin")
 @SuppressWarnings("checkstyle:HideUtilityClassConstructor")
 public class PlyushkinServiceApplication {
 
