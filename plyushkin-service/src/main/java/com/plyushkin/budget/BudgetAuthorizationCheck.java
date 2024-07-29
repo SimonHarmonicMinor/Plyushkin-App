3acf2dc8fe21a3da2d5975340b32d85afed9ec0e
Semyon Kirekov <semyon@kirekov.com>
2024-07-29 23:25:59 +0300

    add BudgetAuthorizationCheck

---
+++ plyushkin-service/src/main/java/com/plyushkin/budget/BudgetAuthorizationCheck.java
@@ -0,0 +1,28 @@
+package com.plyushkin.budget;
+
+import com.plyushkin.user.service.CurrentUserIdProvider;
+import com.plyushkin.wallet.WalletId;
+import com.plyushkin.wallet.repository.WalletRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service("BudgetAuth")
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Slf4j
+public class BudgetAuthorizationCheck {
+    private final CurrentUserIdProvider currentUserIdProvider;
+    private final WalletRepository walletRepository;
+
+    public boolean isWalletCreatedByCurrentUser(WalletId walletId) {
+        final var currentUserId = currentUserIdProvider.get();
+        return walletRepository.findById(walletId)
+                .map(wallet -> wallet.getCreatedBy().equals(currentUserId))
+                .orElseGet(() -> {
+                    log.info("Wallet is not found by id={}", walletId);
+                    return false;
+                });
+    }
+}
