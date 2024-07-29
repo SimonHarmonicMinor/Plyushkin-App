be83028031a32f2616dd48982692748480411748
Semyon Kirekov <semyon@kirekov.com>
2024-07-29 23:53:05 +0300

    fix SecurityConfig

--- plyushkin-service/src/main/java/com/plyushkin/infra/security/SecurityConfig.java
+++ plyushkin-service/src/main/java/com/plyushkin/infra/security/SecurityConfig.java
@@ -1,12 +1,12 @@
 package com.plyushkin.infra.security;
 
-import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,18 +15,16 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableMethodSecurity
 @EnableWebSecurity
+@Slf4j
 public class SecurityConfig {
-    @Bean
-    public SecurityFilterChain httpBasicAuthFilterChain(HttpSecurity http) throws Exception {
-        return http.httpBasic(Customizer.withDefaults())
-                .build();
-    }
+    @Autowired
+    private ApplicationContext applicationContext;
 
     @Bean
-    @SneakyThrows
-    public AuthenticationManager authenticationManager(AuthenticationManagerBuilder builder,
-                                                       AuthenticationProvider authenticationProvider) {
-        return builder.authenticationProvider(authenticationProvider)
+    public SecurityFilterChain httpBasicAuthFilterChain(HttpSecurity http,
+                                                        AuthenticationProvider authenticationProvider) throws Exception {
+        return http.httpBasic(Customizer.withDefaults())
+                .authenticationProvider(authenticationProvider)
                 .build();
     }
 }
