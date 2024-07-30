d9fa46500e5614dc56be669903a353cc7eb7875a
Semyon Kirekov <semyon@kirekov.com>
2024-07-30 22:33:46 +0300

    add WalletRepository.findAllByCreatedBy

--- plyushkin-service/src/main/java/com/plyushkin/wallet/repository/WalletRepository.java
+++ plyushkin-service/src/main/java/com/plyushkin/wallet/repository/WalletRepository.java
@@ -1,8 +1,12 @@
 package com.plyushkin.wallet.repository;
 
+import com.plyushkin.user.UserId;
 import com.plyushkin.wallet.Wallet;
 import com.plyushkin.wallet.WalletId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface WalletRepository extends JpaRepository<Wallet, WalletId> {
+    List<Wallet> findAllByCreatedBy(UserId createdBy);
 }
