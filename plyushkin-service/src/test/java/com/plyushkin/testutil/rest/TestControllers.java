832c603a3cab1a261a4c1b4d76bb852b440918e9
Semyon Kirekov <semyon@kirekov.com>
2024-08-01 00:36:41 +0300

    enhacne TestControllers

--- plyushkin-service/src/test/java/com/plyushkin/testutil/rest/TestControllers.java
+++ plyushkin-service/src/test/java/com/plyushkin/testutil/rest/TestControllers.java
@@ -1,25 +1,48 @@
 package com.plyushkin.testutil.rest;
 
+import com.plyushkin.infra.properties.DefaultUsersProperties;
 import com.plyushkin.openapi.client.ApiClient;
 import com.plyushkin.openapi.client.ExpenseCategoryControllerApi;
+import com.plyushkin.openapi.client.WalletControllerApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestComponent;
 import org.springframework.core.env.Environment;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 @TestComponent
 public class TestControllers {
     @Autowired
     private Environment environment;
+    @Autowired
+    private DefaultUsersProperties defaultUsersProperties;
 
     public ExpenseCategoryControllerApi expenseCategoryController() {
         return new ExpenseCategoryControllerApi(newApiClient());
     }
 
+    public WalletControllerApi walletController() {
+        return new WalletControllerApi(newApiClient());
+    }
+
     private ApiClient newApiClient() {
+        final var user = defaultUsersProperties.values().entrySet().stream().findFirst().orElseThrow().getValue();
         final var apiClient = new ApiClient();
         apiClient.setScheme("http");
         apiClient.setHost("localhost");
         apiClient.setPort(environment.getProperty("local.server.port", int.class));
+        apiClient.setRequestInterceptor(
+                builder ->
+                        builder.header(
+                                "Authorization",
+                                "Basic " +
+                                        new String(Base64.getEncoder().encode(
+                                                (user.username() + ":" + user.password())
+                                                        .getBytes(StandardCharsets.UTF_8)
+                                        ))
+                        )
+        );
         return apiClient;
     }
 }
