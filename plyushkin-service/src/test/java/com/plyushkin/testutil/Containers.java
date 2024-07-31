9cf45605fc372dd8e2ee3fbd5a5a406c231e82d6
Semyon Kirekov <semyon@kirekov.com>
2024-08-01 00:35:43 +0300

    add Containers

---
+++ plyushkin-service/src/test/java/com/plyushkin/testutil/Containers.java
@@ -0,0 +1,7 @@
+package com.plyushkin.testutil;
+
+import org.testcontainers.containers.PostgreSQLContainer;
+
+public class Containers {
+    public static final PostgreSQLContainer<?> POSTGRES = new PostgreSQLContainer<>("postgres:13.5");
+}
