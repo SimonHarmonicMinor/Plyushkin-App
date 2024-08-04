aacf74e434fbb9c97a40bfb0f335ba4a3625ba66
Semyon Kirekov <semyon@kirekov.com>
2024-08-04 23:44:15 +0300

    enhance ExpenseRecordResponse

--- plyushkin-service/src/main/java/com/plyushkin/budget/expense/controller/response/ExpenseRecordResponse.java
+++ plyushkin-service/src/main/java/com/plyushkin/budget/expense/controller/response/ExpenseRecordResponse.java
@@ -3,6 +3,7 @@ package com.plyushkin.budget.expense.controller.response;
 import com.plyushkin.budget.Currency;
 import com.plyushkin.budget.Money;
 import com.plyushkin.budget.expense.ExpenseCategoryNumber;
+import com.plyushkin.budget.expense.ExpenseNumber;
 import com.plyushkin.budget.expense.ExpenseRecord;
 import com.plyushkin.user.UserId;
 import com.plyushkin.wallet.WalletId;
@@ -13,6 +14,8 @@ import java.time.LocalDate;
 public record ExpenseRecordResponse(
         @NotNull
         WalletId walletId,
+        @NotNull
+        ExpenseNumber number,
         @NotNull
         UserId whoDid,
         @NotNull
@@ -29,6 +32,7 @@ public record ExpenseRecordResponse(
     public ExpenseRecordResponse(ExpenseRecord entity) {
         this(
                 entity.getWalletId(),
+                entity.getNumber(),
                 entity.getWhoDid(),
                 entity.getDate(),
                 entity.getCurrency(),
