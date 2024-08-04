3e7b345a42db6e1a460f9b8c099498aac37019fd
Semyon Kirekov <semyon@kirekov.com>
2024-08-04 23:44:04 +0300

    fix ExpenseRecordRepository

--- plyushkin-service/src/main/java/com/plyushkin/budget/expense/repository/ExpenseRecordRepository.java
+++ plyushkin-service/src/main/java/com/plyushkin/budget/expense/repository/ExpenseRecordRepository.java
@@ -1,23 +1,19 @@
 package com.plyushkin.budget.expense.repository;
 
-import com.cosium.spring.data.jpa.entity.graph.domain2.EntityGraph;
-import com.plyushkin.budget.Currency;
 import com.plyushkin.budget.expense.ExpenseCategory;
 import com.plyushkin.budget.expense.ExpenseNumber;
 import com.plyushkin.budget.expense.ExpenseRecord;
 import com.plyushkin.wallet.WalletId;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.Collection;
 import java.util.Optional;
 
 import static jakarta.persistence.LockModeType.PESSIMISTIC_WRITE;
 
-public interface ExpenseRecordRepository extends JpaRepository<ExpenseRecord, Long> {
+public interface ExpenseRecordRepository extends JpaRepository<ExpenseRecord, Long>, JpaSpecificationExecutor<ExpenseRecord> {
     @Query("SELECT w.id FROM Wallet w WHERE w.id = :walletId")
     @Lock(PESSIMISTIC_WRITE)
     void lockByWalletId(WalletId walletId);
@@ -34,8 +30,4 @@ public interface ExpenseRecordRepository extends JpaRepository<ExpenseRecord, Lo
     Optional<ExpenseNumber> findMaxNumberPerWalletId(WalletId walletId);
 
     Optional<ExpenseRecord> findByWalletIdAndNumber(WalletId walletId, ExpenseNumber number);
-
-    Page<ExpenseRecord> findAllByWalletId(WalletId walletId, Pageable pageable, EntityGraph entityGraph);
-
-    Page<ExpenseRecord> findAllByWalletIdAndCurrencyIn(WalletId walletId, Collection<Currency> currency, Pageable pageable, EntityGraph entityGraph);
 }
