1979662afbfe0e6d30f474e9153782c35cc5682e
Семен Киреков <sakireko@mts.ru>
2024-08-29 12:30:20 +0700

    remove menu

--- plyushkin-web-ui/src/app.tsx
+++ plyushkin-web-ui/src/app.tsx
@@ -1,28 +1,22 @@
 import React from "react";
 import "./App.css";
-import { Layout, Menu } from "antd";
+import { Layout } from "antd";
+import { RocketOutlined } from "@ant-design/icons";
 
 const { Header, Content } = Layout;
-
-const items = Array.from({ length: 15 })
-  .fill("")
-  .map((_, index) => ({
-    key: index + 1,
-    label: `nav ${index + 1}`,
-  }));
 
 function App({ children }: { children?: React.ReactNode }) {
   return (
     <Layout>
-      <Header style={{ display: "flex", alignItems: "center" }}>
-        <div className="demo-logo" />
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          defaultSelectedKeys={["2"]}
-          items={items}
-          style={{ flex: 1, minWidth: 0 }}
-        />
+      <Header
+        style={{
+          display: "flex",
+          alignItems: "center",
+          backgroundColor: "gray",
+        }}
+      >
+        <RocketOutlined />
+        Plyushkin Budget Application
       </Header>
       <Content style={{ padding: "0 48px" }}>
         <div
