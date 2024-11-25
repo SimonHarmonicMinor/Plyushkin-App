2549c41e0aac9b0bf53ffcc96bb40337d72a4455
Semyon Kirekov <semyon@kirekov.com>
2024-11-25 23:20:04 +0300

    add MoneyConverter

---
+++ plyushkin-backend/src/main/java/com/plyushkin/domain/value/MoneyConverter.java
@@ -0,0 +1,19 @@
+package com.plyushkin.domain.value;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+import java.math.BigDecimal;
+
+@Converter(autoApply = true)
+class MoneyConverter implements AttributeConverter<Money, BigDecimal> {
+    @Override
+    public BigDecimal convertToDatabaseColumn(Money attribute) {
+        return attribute.value();
+    }
+
+    @Override
+    public Money convertToEntityAttribute(BigDecimal dbData) {
+        return new Money(dbData);
+    }
+}
