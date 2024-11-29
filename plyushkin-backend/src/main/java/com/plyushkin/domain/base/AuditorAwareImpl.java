bd4a527c5eca5f299ca3cfed88f85744a2b5ba47
Semyon Kirekov <semyon@kirekov.com>
2024-11-29 22:10:57 +0300

    fix AuditorAwareImpl

--- plyushkin-backend/src/main/java/com/plyushkin/domain/base/AuditorAwareImpl.java
+++ plyushkin-backend/src/main/java/com/plyushkin/domain/base/AuditorAwareImpl.java
@@ -7,10 +7,11 @@ import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
 class AuditorAwareImpl implements AuditorAware<ID<User>> {
-    private static final long VALUE = 1L;
+    private static final UUID VALUE = UUID.fromString("99723d04-ecd0-48b8-804b-bbb0acda540e");
 
     @Override
     @NonNull
