d10fb8324fc7119d1a2640573a7574448d46fe5c
Semyon Kirekov <semyon@kirekov.com>
2024-11-29 22:11:06 +0300

    add Wallet

--- plyushkin-backend/src/main/java/com/plyushkin/domain/wallet/Wallet.java
+++ plyushkin-backend/src/main/java/com/plyushkin/domain/wallet/Wallet.java
@@ -1,4 +1,29 @@
 package com.plyushkin.domain.wallet;
 
-public class Wallet {
+import com.plyushkin.domain.base.AbstractEntity;
+import com.plyushkin.domain.value.ID;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Table(name = "wallet")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = false)
+@NoArgsConstructor(access = PROTECTED)
+@ToString(onlyExplicitlyIncluded = true)
+public class Wallet extends AbstractEntity<Wallet> {
+    @EmbeddedId
+    @EqualsAndHashCode.Include
+    @ToString.Include
+    private ID<Currency> id;
+
+    @NotNull
+    @ToString.Include
+    private String name;
 }
