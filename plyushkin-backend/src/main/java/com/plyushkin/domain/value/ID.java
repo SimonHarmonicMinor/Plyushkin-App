ab58020e7b41b66ae19754c8853fae6a111d7a2a
Semyon Kirekov <semyon@kirekov.com>
2024-11-29 22:10:50 +0300

    change id type

--- plyushkin-backend/src/main/java/com/plyushkin/domain/value/ID.java
+++ plyushkin-backend/src/main/java/com/plyushkin/domain/value/ID.java
@@ -3,9 +3,11 @@ package com.plyushkin.domain.value;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 
+import java.util.UUID;
+
 @Embeddable
-public record ID<T>(@Column(name = "id") long value) {
-    public static <T> ID<T> now() {
-        return new ID<>(System.currentTimeMillis());
+public record ID<T>(@Column(name = "id") UUID value) {
+    public static <T> ID<T> random() {
+        return new ID<>(UUID.randomUUID());
     }
 }
